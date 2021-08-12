721089b95e0f67268a1ceba6cd4d03df09b36f5f
fksk94 <fksk94@gmail.com>
2021-08-12 20:41:30 +0900

    feat: 🎸 python S 1

--- 210812/1080.py
+++ 210812/1080.py
@@ -6,29 +6,43 @@ begin = [list(input().rstrip()) for i in range(n)]
 end = [list(input().rstrip()) for i in range(n)]
 visit = [[False] * m for i in range(n)]
 res = 0
-if n < 3 or m < 3:
-    print(-1)
-else:
-    for i in range(n-2):
-        for j in range(m-2):
-            if begin[i][j] != end[i][j]:
-                res += 1
-                for ri in range(i, i+3):
-                    for rj in range(j, j + 3):
-                        if begin[ri][rj] == "0":
-                            begin[ri][rj] = "1"
-                        else:
-                            begin[ri][rj] = "0"
-    ch = False
-    for i in range(n):
-        for j in range(m):
-            if begin[i][j] == end[i][j]:
-                continue
-            else:
-                print(-1)
-                ch = True
-                break
-        if ch:
+
+chh = False
+for i in range(n):
+    for j in range(m):
+        if begin[i][j] == end[i][j]:
+            continue
+        else:
+            chh = True
             break
+    if chh:
+        break
+else:
+    print(res)
+if chh:
+    if n < 3 or m < 3:
+        print(-1)
     else:
-        print(res)
+        for i in range(n-2):
+            for j in range(m-2):
+                if begin[i][j] != end[i][j]:
+                    res += 1
+                    for ri in range(i, i+3):
+                        for rj in range(j, j + 3):
+                            if begin[ri][rj] == "0":
+                                begin[ri][rj] = "1"
+                            else:
+                                begin[ri][rj] = "0"
+        ch = False
+        for i in range(n):
+            for j in range(m):
+                if begin[i][j] == end[i][j]:
+                    continue
+                else:
+                    print(-1)
+                    ch = True
+                    break
+            if ch:
+                break
+        else:
+            print(res)
